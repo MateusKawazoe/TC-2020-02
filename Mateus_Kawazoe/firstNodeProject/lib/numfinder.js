3316437061f9f4a104f7c909b3a20c08db54e5c7
Mateuskwz <Matkawazoe@gmail.com>
2021-03-01 15:41:00 -0300

    Atividade 2 (Parte A)

---
+++ Mateus_Kawazoe/firstNodeProject/lib/numfinder.js
@@ -0,0 +1,38 @@
+class NumFinder {
+    
+    constructor() {
+
+    }
+
+    validate(arr) {
+        this.checkLen(arr)
+        this.checkArrayOfNumbers(arr)
+    }
+
+    checkLen(arr) {
+        if(arr.length === 0) 
+            throw 'empty array'
+    }
+
+    checkArrayOfNumbers(arr) {
+        arr.forEach(element => {
+            if(typeof element !== 'number')
+                throw 'invalid array'
+        })
+    }    
+  
+    find(arr) {
+        this.validate(arr)
+        this.biggest = arr[0]
+        this.smallest = arr[0]
+
+        arr.forEach(element => {
+            if(element < this.smallest)
+                this.smallest = element
+            else if(element > this.biggest)
+                this.biggest = element
+        })
+    }
+}
+
+module.exports = NumFinder
---
+++ Mateus_Kawazoe/firstNodeProject/test/numfinder.test.js
@@ -0,0 +1,29 @@
+const NumFinder = require('../lib/numfinder')
+
+describe('NumFinder', () => {
+    const numFinder = new NumFinder()
+
+    test('test a small array', () => {
+        numFinder.find([1, 10])
+        expect(numFinder.biggest).toBe(10)
+        expect(numFinder.smallest).toBe(1)
+    })
+
+    test('test an unordered array', () => {
+        numFinder.find([-99, 5,1, 10, 300, -790, 6])
+        expect(numFinder.biggest).toBe(300)
+        expect(numFinder.smallest).toBe(-790)
+    })
+
+    test('test a empty array', () => {
+        expect(() => {
+            numFinder.find([])
+        }).toThrow('empty array')
+    })
+
+    test('test array with objects and numbers', () => {
+        expect(() => {
+            numFinder.find([1, 2, 3, {nome: 'joao'}, 5, 6])
+        }).toThrow('invalid array')
+    })
+})
