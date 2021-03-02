1c6a82b7a01cc2e4b2040ae9416684c1b276ccc4
Mateuskwz <Matkawazoe@gmail.com>
2021-03-01 21:21:19 -0300

    Exercício 1

---
+++ Mateus_Kawazoe/firstNodeProject/lib/randomnum.js
@@ -0,0 +1,28 @@
+module.exports = class RandomNum {
+
+    constructor() {
+
+    }
+
+    validate(floor, ceiling) {
+        this.smallerThanZero(floor, ceiling)
+        this.floorBiggerThanCeiling(floor, ceiling)
+    }
+
+    floorBiggerThanCeiling(floor, ceiling) {
+        if(floor >= ceiling)
+            throw '-1'
+    }
+
+    smallerThanZero(floor, ceiling) {
+        if(floor < 0 || ceiling < 0)
+            throw '-1'
+    }
+
+    randomNumberGen(floor, ceiling) {
+        this.validate(floor, ceiling)
+        const diff = ceiling - (floor+1)
+
+        this.randomNum = Math.floor(Math.random() * diff) + (floor+1)
+    }
+}
